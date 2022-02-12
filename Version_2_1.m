3f154f79e72320bcae51b2985c199d7a1934be30
Avery-G-Brown <99561389+Avery-G-Brown@users.noreply.github.com>
2022-02-12 14:04:17 -0700

    Update Version_2_1.m
    Local Functions converted to Functions

--- Version_2_1.m
+++ Version_2_1.m
@@ -64,88 +64,17 @@ manualSpectraButton = uicontrol('style','pushbutton','Tag','manualSpectraButton'
 
 %% Functions
 function selectManualSpectra(~,~)                                % begin selectManualSpectra function with no inputs
-prevLine = findall(gcf,'tag','spectraRange');                % find the previous line
-if prevLine ~= 0                                              % if that line exists
-    delete(prevLine);                                            % delete it
-end
-[x,y]=ginput(2);                                             % selection of two points on image
-imagePlace = findall(gcf,'Type','Image');                    % find image location in figure
-currentScalar = imagePlace.UserData;                         % get current image brightness scalar
-data = (imagePlace.CData)/currentScalar;                     % get original data by dividing by scalar
-imageLength = length(data);                                  % get image size
-slope = (y(2)-y(1))/(x(2)-x(1));                             % equation for the line between the two points
-line = slope.*([1:1:imageLength]-x(1))+y(1);                 % create line as a function of x within image length
-hold on                                                      % prevents line from replacing image
-plot(line,'color','Blue','LineWidth',2,'Tag','spectraRange');% create line on plot based on picked positions
-place = findall(gcf,'tag','Spectra');                        % find spectra graph location
-place.XData = [1:1:imageLength];                             % set x to image length
-thing = zeros(1,imageLength);                                % define thing array
-for i = 1:imageLength                                        % for all x values
-    thing(i) = data((round(line(i))),i);                     % set thing to values of image at the line inersects
-end
-place.YData = thing;                                         % plot thing on spectra graph
-place.UserData = thing;                                      % save data in user data as well to preserve original
+    selectManualSpectraFN(1);
 end                                                              % end function
 
 function spectraFinderSliderPressed(~,~)
-prevScat = findall(gcf,'tag','spectra');                         % find the previous scatter plot
-if prevScat ~= 0                                                 % if that exists
-    delete(prevScat);                                            % delete it
-end
-prevLine = findall(gcf,'tag','spectraRange');                    % find the users line
-if isempty(prevLine)                                             % if there is no line
-    error('Select spectra center first')                         % throw error to select spectra first
-end
-obj = gco;                                                       % Get current object (slider)
-val = round(obj.Value);                                          % get value of slider
-lineThing = findall(gcf,'tag','spectraRange');                   % find line location
-linePlace = findall(lineThing,'type','Line');                    % find line within location
-lineVal = linePlace.YData;                                       % retreive y data from line
-spectraSlideVal = findall(gcf,'Tag','spectraFinderSlideVal');    % find slider label location
-spectraSlideVal.String = val;                                    % set slider label to new val
-imagePlace = findall(gcf,'Type','Image');                        % find image location
-currentScalar = imagePlace.UserData;                             % get current image brightness scalar
-data = (imagePlace.CData)/currentScalar;                         % get original data by dividing by scalar
-imageLength = length(data);                                      % get size of image
-lineLength = length(lineVal);                                    % get size of line
-newData = zeros(1,imageLength);                                  % define newData array
-for i = 1:lineLength                                             % for 1:length of line
-    for j = -val:val                                             % j = +- val range
-        newData(round(lineVal(i))+j,i) = data(round(lineVal(i))+j); % get j pixels above and below line and save as newData
-    end
-end
-maxes = zeros(1,imageLength);                                    % define maxes array
-maxesPlaces = zeros(1,imageLength);                              % define maxes places array
-for i=1:imageLength                                              % for 1:length of image
-    maxes(i) = max(newData(:,i));                                % max of newData column save as max of column
-end
-for i=1:imageLength                                              % for 1:length of image
-    maxLoc = find((newData(:,i) == maxes(i)));
-    maxesPlaces(i) = maxLoc(1);
-end
-hold on                                                      % prevents scatter from replacing image
-scatter([1:1:length(data)],maxesPlaces,'filled','Red','Tag','spectra'); % create scatter plot on top of image
-place = findall(gcf,'tag','Spectra');                        % find spectra plot location
-place.XData = [1:1:imageLength];                             % set x axis to image size
-imageMaxes = zeros(1,imageLength);                           % define imageMaxes array
-for i = 1:imageLength                                        % for 1: length of image
-    imageMaxes(i) = data(maxesPlaces(i),i);                  % get data of max locations and save to imageMaxes
-end
-place.YData = imageMaxes;                                    % set y data of spectra plot to imageMaxes
-place.UserData = imageMaxes;                                 % set user data of spectra plot to imageMaxes
+    spectraFinderSliderPressedFN(1);
 end                                                          % end function
 
 function smoothSliderPressed(~,~)                            % begin smoothButtonPressed function with no inputs
-obj = gco;                                                   % get current object (slider)
-val = round(obj.Value,0);                                    % get value of slider rounded to nearest whole number
-spectraPlace = findall(gcf,'tag','Spectra');                 % find spectra plot location
-data = spectraPlace.UserData;                                % get "unsmoothed" data from user data of spectra plot
-smoothSlideVal = findall(gcf,'Tag','smoothSlideVal');        % find label for slider
-smoothSlideVal.String = val;                                 % set label to new slider value
-smoothed = smoothdata(data,'gaussian',val);                  % smooth data using guassian function with a domain of val
-spectraPlace.YData = smoothed;                               % set spectra plot y data to smoothed data
-end                                                              % end function
+    smoothSliderPressedFN(1);
+end
 
- function brightnessSliderPressed(~,~)                        % begin brightnessSliderPressed
-     brightnessSliderPressedFN(1);
- end
+function brightnessSliderPressed(~,~)                        % begin brightnessSliderPressed
+    brightnessSliderPressedFN(1);
+end
